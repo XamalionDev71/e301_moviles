0a59876efe8a9f02ed79e92fcc0d8498e84a7928
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-03-28 09:08:37 -0600

    commit 1 28Mar2025

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e301_moviles/config/menu/menu_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,9 +7,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Widgets en Flutter'),
-      ),
+      appBar: AppBar(title: Text('Widgets en Flutter')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      leading: Icon(menuItem.icon,color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded),
+      onTap: () {
+        //TODO: navegar a otras pantallas
+      },
     );
   }
 }
