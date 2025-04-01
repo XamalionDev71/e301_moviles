0ea92db088197f21b160402af5565bd0208efa65
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-01 10:08:09 -0600

    commit 3 1Abril2025

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,10 @@
+export 'package:e301_moviles/presentation/screens/animated/animated_screen.dart';
+export 'package:e301_moviles/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:e301_moviles/presentation/screens/controls_ui/controls_ui_screen.dart';
+export 'package:e301_moviles/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:e301_moviles/presentation/screens/progress/progress_screen.dart';
+export 'package:e301_moviles/presentation/screens/snackbar/snackbar_screen.dart';
 export 'package:e301_moviles/presentation/screens/buttons/buttons_screen.dart';
 export 'package:e301_moviles/presentation/screens/cards/cards_screen.dart';
 export 'package:e301_moviles/presentation/screens/home/home_screen.dart';
+
