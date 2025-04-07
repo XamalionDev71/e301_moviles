ed550d4d60cbe94a6ed8ed2238138845a5271555
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-07 08:17:01 -0600

    commit 2 7Abr2025

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -7,6 +7,41 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Progress Indicator'),
+      ),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          Text('Circular Progress Indicator'),
+          SizedBox(height: 10,),
+          CircularProgressIndicator(
+            strokeWidth: 4,
+            backgroundColor: Colors.black12,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
