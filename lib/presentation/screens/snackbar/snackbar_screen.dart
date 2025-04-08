2ed8dc8350f340bc4f56d94321b7fac0822647e2
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-08 09:10:37 -0600

    commit2 8Abr2025

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,14 +5,31 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text('Hola Mundo!'),
+      action: SnackBarAction(label: 'Ok', onPressed: () {}),
+      duration: Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Snackbars/Diálogos'),
+      appBar: AppBar(title: Text('Snackbars/Diálogos')),
+      body: Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){},
+        onPressed: () {
+          showCustomSnackbar(context);
+        },
         label: Text('Mostrar Snackbar'),
         icon: Icon(Icons.remove_red_eye_outlined),
       ),
