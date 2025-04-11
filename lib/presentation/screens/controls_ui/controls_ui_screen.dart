b6390d5f8f993dd96c8f0bbc1bdae8eed30789f9
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-11 09:17:10 -0600

    commit3 11Abr2025

--- lib/presentation/screens/controls_ui/controls_ui_screen.dart
+++ lib/presentation/screens/controls_ui/controls_ui_screen.dart
@@ -21,8 +21,11 @@ class _UiControlsView extends StatefulWidget {
   State<_UiControlsView> createState() => _UiControlsViewState();
 }
 
+enum Transportation { car, plane, boat, submarine }
+
 class _UiControlsViewState extends State<_UiControlsView> {
   bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +38,50 @@ class _UiControlsViewState extends State<_UiControlsView> {
           onChanged: (value) {
             setState(() {
               isDeveloper = !isDeveloper;
+            });
+          },
+        ),
+        RadioListTile(
+          title: Text('By Car'),
+          subtitle: Text('Viajar por carro'),
+          value: Transportation.car,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.car;
+            });
+          },
+        ),
+        RadioListTile(
+          title: Text('By Plane'),
+          subtitle: Text('Viajar por avión'),
+          value: Transportation.plane,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.plane;
+            });
+          },
+        ),
+        RadioListTile(
+          title: Text('By Boat'),
+          subtitle: Text('Viajar por barco'),
+          value: Transportation.boat,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.boat;
+            });
+          },
+        ),
+        RadioListTile(
+          title: Text('By Submarine'),
+          subtitle: Text('Viajar por submarino'),
+          value: Transportation.submarine,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.submarine;
             });
           },
         ),
