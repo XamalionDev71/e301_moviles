75196e94767a2f90355ebffc513ee28adefc4c70
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-29 09:26:28 -0600

    commit 3 29Abr2025

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const String name = 'inifinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(title: Text('InfiniteScroll')),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Text('#TeamSistemas');
-        },
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage(
+                "https://picsum.photos/id/${imagesIds[index]}/500/300",
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: Icon(Icons.arrow_back_outlined),
       ),
     );
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+
+    // imagesIds.add(lastId + 1);
+    // imagesIds.add(lastId + 2);
+    // imagesIds.add(lastId + 3);
+    // imagesIds.add(lastId + 4);
+    // imagesIds.add(lastId + 5);
+
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
   }
 }
